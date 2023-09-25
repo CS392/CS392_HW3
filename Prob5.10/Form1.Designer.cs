3d71251aa17a188d17fc2077e8ab83065ead4474
Hengsheng-Liu <lhs7714@bu.edu>
2023-09-25 19:39:29 -0400

    finish the 5.10

--- Prob5.10/Form1.Designer.cs
+++ Prob5.10/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            GuessLabel = new Label();
+            button1 = new Button();
+            Input = new TextBox();
+            Hint = new Label();
+            randomNum = new Label();
+            SuspendLayout();
+            // 
+            // GuessLabel
+            // 
+            GuessLabel.AutoSize = true;
+            GuessLabel.Location = new Point(248, 141);
+            GuessLabel.Name = "GuessLabel";
+            GuessLabel.Size = new Size(267, 20);
+            GuessLabel.TabIndex = 0;
+            GuessLabel.Text = "Enter a number from 1 to 100 to guess!";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(303, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Guess";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Input
+            // 
+            Input.Location = new Point(288, 203);
+            Input.Name = "Input";
+            Input.Size = new Size(125, 27);
+            Input.TabIndex = 2;
+            Input.TextChanged += Input_TextChanged;
+            // 
+            // Hint
+            // 
+            Hint.AutoSize = true;
+            Hint.Location = new Point(328, 170);
+            Hint.Name = "Hint";
+            Hint.Size = new Size(0, 20);
+            Hint.TabIndex = 3;
+            // 
+            // randomNum
+            // 
+            randomNum.AutoSize = true;
+            randomNum.Location = new Point(461, 205);
+            randomNum.Name = "randomNum";
+            randomNum.Size = new Size(0, 20);
+            randomNum.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(randomNum);
+            Controls.Add(Hint);
+            Controls.Add(Input);
+            Controls.Add(button1);
+            Controls.Add(GuessLabel);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label GuessLabel;
+        private Button button1;
+        private TextBox Input;
+        private Label Hint;
+        private Label randomNum;
     }
 }
--- Prob5.10/Form1.cs
+++ Prob5.10/Form1.cs
@@ -1,10 +1,49 @@
+using System;
 namespace Prob5._10
 {
     public partial class Form1 : Form
     {
+        int num;
+        Random random = new Random();
+        int randomNumber;
         public Form1()
         {
             InitializeComponent();
+            randomNumber = random.Next(1, 101);
+            randomNum.Text = "The right answer is(for testing)" + randomNumber.ToString();
+        }
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                num = int.Parse(Input.Text);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format. Cannot convert to integer.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow. The number is too large to fit in an int.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (num == randomNumber)
+            {
+                Hint.Text = "You got it!";
+                randomNumber = random.Next(1, 101);
+                randomNum.Text = "The right answer is(for testing) " + randomNumber.ToString();
+            }
+            else if (num < randomNumber)
+            {
+                Hint.Text = "Too Low";
+            }
+            else
+            {
+                Hint.Text = "Too High";
+            }
         }
     }
 }
