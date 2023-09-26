5e852eee9e8ffafb0afa791160efd843ca597582
Rong Chen <rongc0723@gmail.com>
2023-09-25 21:05:57 -0400

    5.11

--- Prob5.11/Form1.Designer.cs
+++ Prob5.11/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            resultTextBox = new Label();
+            errorLabel = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(129, 178);
+            label1.Name = "label1";
+            label1.Size = new Size(155, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Enter a nonnegative integer:";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(307, 170);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(242, 224);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Result";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(307, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Calculate Factorial";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // resultTextBox
+            // 
+            resultTextBox.AutoSize = true;
+            resultTextBox.Location = new Point(307, 224);
+            resultTextBox.Name = "resultTextBox";
+            resultTextBox.Size = new Size(0, 15);
+            resultTextBox.TabIndex = 4;
+            // 
+            // errorLabel
+            // 
+            errorLabel.AutoSize = true;
+            errorLabel.Location = new Point(433, 173);
+            errorLabel.Name = "errorLabel";
+            errorLabel.Size = new Size(0, 15);
+            errorLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(errorLabel);
+            Controls.Add(resultTextBox);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
+        private Label resultTextBox;
+        private Label errorLabel;
     }
 }
--- Prob5.11/Form1.cs
+++ Prob5.11/Form1.cs
@@ -6,5 +6,28 @@ namespace Prob5._11
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = Int32.Parse(textBox1.Text);
+            if (number < 0)
+            {
+                errorLabel.Text = "Please enter a nonnegative integer";
+                return;
+            }
+            int result = 1;
+            while (number > 0)
+            {
+                result *= number;
+                number--;
+            }
+            resultTextBox.Text = result.ToString();
+            errorLabel.Text = "";
+        }
     }
 }
